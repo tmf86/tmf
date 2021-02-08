01c00f4134579a66058cab4a9dac49b7275f0200
samuel-assemien <assemiensamuel48@gmail.com>
2021-02-08 19:08:00 +0000

    05-02-2021 suppression de la propiete errormessagesCustumized

--- Repository/RegiserRepositoryValidator.php
+++ Repository/RegiserRepositoryValidator.php
@@ -7,21 +7,25 @@ namespace Repository;
 class RegiserRepositoryValidator extends ValidatorCustumer
 {
     protected $messagesCustomized = [
-        'Nom' => "nom ",
-        "Email" => "email"
+        "Email" => "email",
+        "Contact" => "contact",
+        "Prenom" => "prenom"
     ];
     protected $errorsMessages = [
-        'required' => 'le :attribute est obligatoire.',
-        'min' => 'désolé le :attribute doit contenir au moins 10 caractères.',
-        "alpha" => 'seul les caractères aphabétique sont pris en compte.',
-        'email' => 'l\':attribute est invalide.'
+        'required' => ':attribute obligatoire.',
+        'min' => ':attribute invalide',
+        "max" => ":attribute doit contenir au plus 10 chiffres",
+        "alpha" => ':attribute invalide.',
+        'email' => ':attribute obligatoire.'
     ];
     /**
      * @var string[]
      */
     protected $rules = [
-        'nom' => 'required|min:10|alpha',
-        'email' => 'required|email',
+        'nom' => 'required|min:3|alpha',
+        'prenom' => 'required|min:3|alpha',
+        'contact' => 'required|numeric|regex:#^[0-9]([-. ]?[0-9]{2}){4}$#',
+        "email" => "required"
     ];
 
 }
