6852424d317414df58aa68fb7463df6e9ac40513
samuel-assemien <assemiensamuel48@gmail.com>
2021-02-08 20:11:27 +0000

    ajout de la regle phone et de son message personnalise

--- Repository/RegiserRepositoryValidator.php
+++ Repository/RegiserRepositoryValidator.php
@@ -6,25 +6,21 @@ namespace Repository;
 
 class RegiserRepositoryValidator extends ValidatorCustumer
 {
-    protected $messagesCustomized = [
-        "Email" => "email",
-        "Contact" => "contact",
-        "Prenom" => "prenom"
-    ];
     protected $errorsMessages = [
         'required' => ':attribute obligatoire.',
+        'numeric'=>':attribute invalide.',
         'min' => ':attribute invalide',
-        "max" => ":attribute doit contenir au plus 10 chiffres",
         "alpha" => ':attribute invalide.',
-        'email' => ':attribute obligatoire.'
+        'email' => ':attribute obligatoire.',
+        'phone' => ':attribute invalide'
     ];
     /**
      * @var string[]
      */
     protected $rules = [
-        'nom' => 'required|min:3|alpha',
-        'prenom' => 'required|min:3|alpha',
-        'contact' => 'required|numeric|regex:#^[0-9]([-. ]?[0-9]{2}){4}$#',
+        'nom' => 'required|min:2|alpha',
+        'prenom' => 'required|min:2|alpha',
+        'contact' => 'required|numeric|phone',
         "email" => "required"
     ];
 
