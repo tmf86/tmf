3f0d6795d20ca4d032e2b4537bccd08b50962330
samuel-assemien <assemiensamuel48@gmail.com>
2021-02-08 21:12:38 +0000

    mise a jour

--- controller/Controller.php
+++ controller/Controller.php
@@ -6,21 +6,13 @@ namespace Contoller;
 
 use View\View;
 
-class Controller implements Request\Request
+class Controller 
 {
 
-    public function __construct()
-    {
-        $this->postRequest();
-    }
 
     protected function load_views(string $view, array $vars = [])
     {
         return new View($view, $vars);
     }
 
-    public function postRequest()
-    {
-        // TODO: Implement postRequest() method.
-    }
 }
