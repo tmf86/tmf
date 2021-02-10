f9ac0b33680adba4bbf265dc218f706d83fe938c
samuel-assemien <assemiensamuel48@gmail.com>
2021-02-10 21:07:25 +0000

    problem avec l'url de la requete ajax regle

--- public/js/register.js
+++ public/js/register.js
@@ -2,27 +2,25 @@ $(function () {
     $("#form-register").submit(function (e) {
         e.preventDefault()
         $.ajax({
-            url: "http://localhost/Cpy-Mvc/registerStore",
+            url: buildUrl("registerStore"),
             type: "post",
             data: $(this).serialize(),
             dataType: "json",
             success: function (data) {
-                alert("ok")
                 console.log(data)
             },
             error: function (xhr) {
-                // alert(xhr.responseText)
-                // console.log(xhr)
-                // const errors = JSON.parse(xhr.responseText);
-                // console.log(errors)
-                // if (errors.code === 0) {
-                //     for (const property in errors) {
-                //         $(`label[for='${property}'] small`).html(errors[property])
-                //         $(`input[name="${property}"]`).addClass("error")
-                //         $(`input[name="${property}"] ~ span.icon `).addClass("error")
-                //         console.log(`${property}: ${errors[property]}`);
-                //     }
-                // }
+                console.log(xhr)
+                const errors = xhr.responseJSON;
+                console.log(errors)
+                if (errors.code === 0) {
+                    for (const property in errors) {
+                        $(`label[for='${property}'] small`).html(errors[property])
+                        $(`input[name="${property}"]`).addClass("error")
+                        $(`input[name="${property}"] ~ span.icon `).addClass("error")
+                        console.log(`${property}: ${errors[property]}`);
+                    }
+                }
             }
         })
     })
