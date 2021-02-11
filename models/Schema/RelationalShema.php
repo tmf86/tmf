943b5a4b551f408b8892af3f242320a48472c80b
samuel-assemien <assemiensamuel48@gmail.com>
2021-02-11 21:09:54 +0000

    mise a jour de la logique des relations

---
+++ models/Schema/RelationalShema.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace Model\Shema;
+
+
+trait RelationalShema
+{
+    protected $foreignkey;
+    protected $foreignTable;
+    protected $foreignTableKey;
+    private $relationQuery = "";
+
+    /**
+     * @param int $id
+     * @param int $cardinality
+     * @return $this
+     */
+    private function relationShip(int $id, int $cardinality)
+    {
+        if ($cardinality == 1) {
+            $relationship = "select * from $this->table inner join $this->foreignTable on $this->table.$this->foreignkey = $this->foreignTable.$this->foreignTableKey where $this->primaryKeyStr = $id  limit 1";
+            $this->relationQuery = $relationship;
+        } else if ($cardinality == 2) {
+            $relationship = "select * from $this->table inner join $this->foreignTable on $this->table.$this->primaryKeyStr = $this->foreignTable.$this->foreignkey where $this->primaryKeyStr = $id ";
+            $this->relationQuery = $relationship;
+            var_dump($relationship);
+        }
+        return $this;
+    }
+
+    /**
+     * @param $table
+     * @return mixed
+     */
+    protected function hasMany($table)
+    {
+        $this->self = $table;
+        return $this->relationShip($this->{$this->primaryKeyStr}, 2)->query($this->relationQuery, true);
+    }
+
+    /**
+     * @param $table
+     * @return mixed
+     */
+    protected function belongTo($table)
+    {
+        $this->self = $table;
+        return $this->relationShip($this->{$this->primaryKeyStr}, 1)->query($this->relationQuery);
+    }
+}
