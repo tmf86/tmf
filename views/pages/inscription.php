4d93c597ef9f2608d784ac6b9669d67dd5694a50
samuel-assemien <assemiensamuel48@gmail.com>
2021-02-17 18:58:11 +0000

    ajout du loader de soumission

--- views/pages/inscription.php
+++ views/pages/inscription.php
@@ -121,3 +121,16 @@
     </div>
 </div>
 <!--Success Modal-->
+<div id="debug"></div>
+<div class="cloud"></div>
+<div class="loader-conatiner">
+    <!--    <div class="spinner-grow" role="status">-->
+    <!--        <span class="sr-only">Loading...</span>-->
+    <!--    </div>-->
+    <div class="spinner-border text-primary rezise" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>
+    <div class="loader-label">
+        Patientez<span id="point-loader" style="font-size:1.7rem">.</span>
+    </div>
+</div>
