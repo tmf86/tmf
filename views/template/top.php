22187c6eb70359e4c7d7c0393618b0052ab77c35
samuel-assemien <assemiensamuel48@gmail.com>
2021-02-21 03:31:50 +0000

    fix : logo arrondi font transparent

--- views/template/top.php
+++ views/template/top.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="image/jpeg" href="images/cpy.jpeg"/>
+    <link rel="icon" type="image/png" href="images/logo.png"/>
     <!-- Stylesheet   -->
     <link rel="stylesheet" href="public/css/b-and-mb.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
