483f8cceacb6ed4184a5c40b15f8bf042ea6f8d3
samuel-assemien <assemiensamuel48@gmail.com>
2021-02-22 00:06:44 +0000

    fix: footer z-index problem

--- views/template/bottom.php
+++ views/template/bottom.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
-<footer class="page-footer font-small special-color pt-4">
+<footer class="page-footer font-small special-color pt-4 fixed-bottom z-after-loader">
     <div class="container-fluid text-center text-md-left">
         <div class="row">
             <div class="col-md-6 mt-md-0 mt-3">
