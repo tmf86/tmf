ba259e717a2ec1047002fc5bca69bdb9e414262b
samuel-assemien <assemiensamuel48@gmail.com>
2021-02-22 20:59:16 +0000

    finalisation de la partie frontend

--- views/pages/finalize_account_creation.php
+++ views/pages/finalize_account_creation.php
@@ -7,24 +7,18 @@
             </p>
             <div class="underline"></div>
         </div>
-        <div>
-            <!--            <div class="position-relative">-->
-            <!--                <span class="password-eye"><i class="fas fa-eye"></i></i></span>-->
-            <!--                <input type="text" name="username" value="fake_username" autocomplete="new-username" class="d-none">-->
-            <!--            </div>-->
+        <div class="mt-5">
             <div class="position-relative">
-                <span class="password-eye first eye-hide"><i class="fas fa-eye"></i></span>
+                <span class="password-eye first eye-hide"><i class="fas fa-eye-slash"></i></i></span>
                 <input type="password" id="password" class="form-control mb-4 fm-p" name="password"
                        placeholder="Mot de passe" autocomplete="new-password">
             </div>
             <div class="position-relative">
-                <span class="password-eye second eye-hide"><i class="fas fa-eye"></i></span>
+                <span class="password-eye second eye-hide"><i class="fas fa-eye-slash"></i></i></span>
                 <input type="password" id="password_verify" class="form-control mb-4 fm-p" name="password_verify"
                        placeholder="Retappez le mot de passe" autocomplete="new-password">
             </div>
         </div>
-        <div class="fm-btn-container">
-            <button class="btn btn-info btn-block fm-btn" type="submit">Suivant</button>
-        </div>
+        <button class="btn btn-info btn-block fm-btn" type="submit">Suivant</button>
     </form>
 </div>
