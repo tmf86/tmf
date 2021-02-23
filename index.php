e07ac96ab3bb6a1e43d95025cf8ffe9623f74881
samuel-assemien <assemiensamuel48@gmail.com>
2021-02-23 00:11:50 +0000

    mise ajour : reformatage des lignes du code et implementation des changements de la configuration

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Contoller\ConnexionController;
+use Contoller\FinalizeAccountController;
 use Contoller\FormationController;
 use Contoller\HomeController;
 use Contoller\Http\Request;
@@ -8,32 +9,91 @@ use Contoller\RegisterController;
 use Contoller\SujetController;
 use Repositories\Mailer;
 
+
 require "vendor/autoload.php";
+require 'config/config.php';
 require "helpers/helper.php";
 session_start();
+
+/*
+ *  NB : Si j'allais jamais il est question de créer un controller qui a besoin a la fois de variable envoyé
+    par la methode GET et aussi de variable passé depuis la route voici l'ordre dans la quelle les paramettres de
+    Cette methode du controller doivent - être definis : --->($variables_envoyé_depuis_la_route,$variales_envoyé_depuis,la_methode_get)
+*/
+
 $dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
-    $r->addRoute('GET', '/Cpy-Mvc/', ["class" => HomeController::class, "method" => "index"]);
-    $r->addRoute('GET', '/Cpy-Mvc/connexion', ["class" => ConnexionController::class, "method" => "index"]);
-    $r->addRoute('GET', '/Cpy-Mvc/acceuil', ["class" => HomeController::class, "method" => "index"]);
-    $r->addRoute('GET', '/Cpy-Mvc/inscription', ["class" => RegisterController::class, "method" => "index"]);
-    $r->addRoute('POST', '/Cpy-Mvc/registerStore',
-        ["class" => RegisterController::class, "method" => "registerStore", "vars" =>
-            [
-                new Request()
-            ]
+    $r->addRoute('GET', '/Cpy-Mvc/',
+        [
+            'class' => HomeController::class,
+            'method' => 'index'
         ]);
-    $r->addRoute(['GET','POST'], '/Cpy-Mvc/finalize_account_creation', function () {
-//        /{email:[A-Za-z0-9]+}
-//        var_dump($_POST);
-        $scripts = [
-            sprintf("<script src='%spublic/js/functions.js'></script>", rootUrl()),
-            sprintf("<script src='%spublic/js/script.js'></script>", rootUrl())
-        ];
-        return new View\View("pages.finalize_account_creation", compact('scripts'));
+    $r->addRoute('GET', '/Cpy-Mvc/connexion',
+        [
+            'class' => ConnexionController::class,
+            'method' => 'index'
+        ]);
+    $r->addRoute('GET', '/Cpy-Mvc/acceuil',
+        [
+            'class' => HomeController::class,
+            'method' => 'index'
+        ]);
+    $r->addRoute('GET', '/Cpy-Mvc/inscription',
+        [
+            'class' => RegisterController::class,
+            'method' => 'index'
+        ]);
+    $r->addRoute('POST', '/Cpy-Mvc/registerStore',
+        [
+            'class' => RegisterController::class,
+            'method' => 'registerStore',
+            'vars' =>
+                [
+                    new Request()
+                ]
+        ]);
+    $r->addGroup('/Cpy-Mvc/finalize_account_creation/', function (FastRoute\RouteCollector $r) {
+
+//        $r->addRoute(['GET', 'POST'], '{email:[A-Za-z0-9]+}', function ($email) {
+////        var_dump($_POST);
+//            $scripts = [
+//                sprintf("<script src='%spublic/js/functions.js'></script>", rootUrl()),
+//                sprintf("<script src='%spublic/js/script.js'></script>", rootUrl())
+//            ];
+//            return new View\View("pages.finalize_account_creation", compact('scripts'));
+//        });
+        $r->addRoute('GET', '{email:[A-Za-z0-9]+}',
+            [
+                'class' => FinalizeAccountController::class,
+                'method' => 'index',
+                'gets' => true,
+                'vars' =>
+                    [
+                        new Request()
+                    ]
+
+            ]);
+//            $scripts = [
+//                sprintf("<script src='%spublic/js/functions.js'></script>", rootUrl()),
+//                sprintf("<script src='%spublic/js/script.js'></script>", rootUrl())
+//            ];
+//            return new View\View("pages.finalize_account_creation", compact('scripts'));
+
     });
-    $r->addRoute('GET', '/Cpy-Mvc/sujets', ["class" => SujetController::class, "method" => "index"]);
-    $r->addRoute('GET', '/Cpy-Mvc/cours', ["class" => FormationController::class, "method" => "index"]);
-    $r->addRoute('GET', '/Cpy-Mvc/videos_formation', ["class" => FormationController::class, "method" => "index"]);
+    $r->addRoute('GET', '/Cpy-Mvc/sujets',
+        [
+            'class' => SujetController::class,
+            'method' => 'index'
+        ]);
+    $r->addRoute('GET', '/Cpy-Mvc/cours',
+        [
+            'class' => FormationController::class,
+            'method' => 'index'
+        ]);
+    $r->addRoute('GET', '/Cpy-Mvc/videos_formation',
+        [
+            'class' => FormationController::class,
+            'method' => 'index'
+        ]);
     $r->addRoute('GET', '/Cpy-Mvc/test', function () {
         $mailer = new Mailer("kofi jack", 'assemiensamuel48@gmail.com');
         $mailer->mail(new Request());
