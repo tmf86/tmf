3f607624d207eed24534d4032286df4f59ddec3b
samuel-assemien <assemiensamuel48@gmail.com>
2021-02-23 04:39:12 +0000

    verification de la validie de l'url

--- controllers/FinalizeAccountController.php
+++ controllers/FinalizeAccountController.php
@@ -5,6 +5,7 @@ namespace Contoller;
 
 
 use Contoller\Http\Request;
+use Model\User;
 
 class FinalizeAccountController extends Controller
 {
@@ -13,21 +14,43 @@ class FinalizeAccountController extends Controller
      * @param string $email
      * @return \View\View
      */
-    public function index(string $email)
+    public function index(string $id, string $email)
     {
-        $scripts = [
-            sprintf("<script src='%spublic/js/functions.js'></script>", rootUrl()),
-            sprintf("<script src='%spublic/js/script.js'></script>", rootUrl())
-        ];
+        if (!$this->urlValidate($id, $email)) {
+            Request::abort(404);
+            exit();
+        }
+        $scripts =
+            [
+                sprintf("<script src='%spublic/js/functions.js'></script>", rootUrl()),
+                sprintf("<script src='%spublic/js/script.js'></script>", rootUrl())
+            ];
         return $this->load_views("pages.finalize_account_creation", compact('scripts'));
+
     }
 
     /**
      * @param Request $request
      * @param string $email
      */
-    public function accountStore(Request $request, string $email)
+    public function accountStore(Request $request, string $id, string $email)
     {
 
+    }
+
+    /**
+     * @param string $id
+     * @param string $email
+     * @return bool
+     */
+    private function urlValidate(string $id, string $email)
+    {
+        $user = new User();
+        $bool = false;
+        $user = $user->query(sprintf("select * from membre where email='%s'", $email));
+        if ($user) {
+            $bool = buildUniqueID($user->mat_membre) === $id;
+        }
+        return $bool;
     }
 }
