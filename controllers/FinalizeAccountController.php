3104dc75926a8bd67b1ad862c9eedcc51f230f3b
samuel-assemien <assemiensamuel48@gmail.com>
2021-02-23 00:12:27 +0000

    controller de finalisation de la creation du compte

---
+++ controllers/FinalizeAccountController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Contoller;
+
+
+use Contoller\Http\Request;
+
+class FinalizeAccountController extends Controller
+{
+
+    public function index(Request $request, string $email)
+    {
+        echo "ok";
+    }
+}
