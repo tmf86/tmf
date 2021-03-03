2e46d35c81b12549772eb304bbda83e88c4fe7f4
samuel-assemien <assemiensamuel48@gmail.com>
2021-03-03 02:01:33 +0000

    mise a jour du formulaire de connexion

--- views/pages/login.php
+++ views/pages/login.php
@@ -1,28 +1,34 @@
-<div class="container mb-5 mt-5">
-    <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-8">
-            <div class="card rgba-white-strong">
-                <div class="card-body">
-                    <form>
-                        <p class="h4 text-center py-4 text-uppercase border-bottom border-info">Connexion</p>
-                        <div class="md-form">
-                            <i class="fa fa-envelope prefix grey-text"></i>
-                            <input type="email" id="materialFormCardEmailEx" class="form-control">
-                            <label for="materialFormCardEmailEx" class="font-weight-light">Identifiant</label>
-                        </div>
-                        <div class="md-form">
-                            <i class="fa fa-lock prefix grey-text"></i>
-                            <input type="password" id="materialFormCardPasswordEx" class="form-control">
-                            <label for="materialFormCardPasswordEx" class="font-weight-light">Mot de passe</label>
-                        </div>
-                        <div class="text-center py-4 mt-3">
-                            <button class="btn btn-cyan" type="submit">Se connecter</button>
-                        </div>
-                    </form>
-                </div>
+<!--<div class="container mb-5 mt-5">-->
+<!--    <div class="row">-->
+<!--        <div class="col-md-4"></div>-->
+<!--        <div class="col-md-4">-->
+<!--            <div class="rgba-white-strong">-->
+<!--                -->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="col-md-4"></div>-->
+<!--    </div>-->
+<!--</div>-->
+<div class="form-container mt-4 p-3">
+    <div class="col-md-5">
+        <form id="login-form">
+            <p class="h4 text-center py-4 text-uppercase font-italic" style="text-decoration: underline">
+                Connexion</p>
+            <div class="md-form">
+                <i class="fa fa-envelope prefix grey-text"></i>
+                <input type="email" id="materialFormCardEmailEx" class="form-control">
+                <label for="materialFormCardEmailEx" class="font-weight-light">Identifiant ou
+                    Email</label>
             </div>
-        </div>
-        <div class="col-md-2"></div>
+            <div class="md-form">
+                <i class="fa fa-lock prefix grey-text"></i>
+                <input type="password" id="materialFormCardPasswordEx" class="form-control">
+                <label for="materialFormCardPasswordEx" class="font-weight-light">Mot de
+                    passe</label>
+            </div>
+            <div class="text-center py-4 mt-3">
+                <button class="btn btn-cyan font-italic" id="login" type="submit">Se connecter</button>
+            </div>
+        </form>
     </div>
 </div>
