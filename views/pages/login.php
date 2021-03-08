df02b982049935bed02d0cae765eed1a159be837
samuel-assemien <assemiensamuel48@gmail.com>
2021-03-08 12:04:45 +0000

    mise a jour : message d'erreur et de succes

--- views/pages/login.php
+++ views/pages/login.php
@@ -1,35 +1,42 @@
-<!--<div class="container mb-5 mt-5">-->
-<!--    <div class="row">-->
-<!--        <div class="col-md-4"></div>-->
-<!--        <div class="col-md-4">-->
-<!--            <div class="rgba-white-strong">-->
-<!--                -->
-<!--            </div>-->
-<!--        </div>-->
-<!--        <div class="col-md-4"></div>-->
-<!--    </div>-->
-<!--</div>-->
-<div class="form-container mt-4 p-3">
-    <div class="col-md-5">
-        <form id="form-login">
+<div class="form-container mt-5 p-2">
+    <div class="col-md-6 card p-5">
+        <form id="form-login" class="p-3 w-100">
             <p class="text-center mb-4">
                 <span class="title-custum text-uppercase">connexion</span>
             </p>
-            <div id="error-alert"></div>
-            <div class="md-form">
-                <i class="fa fa-envelope prefix grey-text"></i>
-                <input type="email" id="email_ou_identifiant" name="email_ou_identifiant" class="form-control">
-                <label for="email_ou_identifiant" class="font-weight-light">Identifiant ou
-                    Email</label>
+            <div id="alert" class="mt-5"></div>
+            <div class="sm-m">
+                <div class="md-form mt-3">
+                    <i class="fa fa-envelope prefix grey-text" data-name="email_ou_identifiant"></i>
+                    <input type="text" id="email_ou_identifiant" name="email_ou_identifiant" class="form-control">
+                    <label for="email_ou_identifiant" class="font-weight-light">Identifiant ou
+                        Email</label>
+                </div>
+                <div class="md-form mb-2">
+                    <i class="fa fa-lock prefix grey-text" data-name="mot_de_passe"></i>
+                    <input type="password" id="mot_de_passe" name="mot_de_passe" class="form-control">
+                    <label for="mot_de_passe" class="font-weight-light">Mot de
+                        passe</label>
+                </div>
             </div>
-            <div class="md-form">
-                <i class="fa fa-lock prefix grey-text"></i>
-                <input type="password" id="mot_de_passe" name="mot_de_passe" class="form-control">
-                <label for="mot_de_passe" class="font-weight-light">Mot de
-                    passe</label>
-            </div>
-            <button class="btn btn-info btn-block my-4 fm-btn font-italic" id="login" type="submit">S'Inscrire
+            <button class="btn btn-info btn-block my-4 fm-btn font-italic" id="login" type="submit"
+                    style="font-weight: 300">se connecter
             </button>
         </form>
     </div>
 </div>
+<div class="cipy-loader-container">
+    <div class="loader-center">
+        <div class="loader-d-flex">
+            <div>
+                <span class="cipy-loader"></span>
+            </div>
+            <div>
+                <div class="round nb"></div>
+                <div class="round nb-1"></div>
+                <div class="round nb-2"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="debug"></div>
