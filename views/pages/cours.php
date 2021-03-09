14027568673b87794f07472c3068ed67081cdc4e
samuel-assemien <assemiensamuel48@gmail.com>
2021-03-09 11:43:07 +0000

    fix : problem d'inegration de youtube

--- views/pages/cours.php
+++ views/pages/cours.php
@@ -1,27 +1,185 @@
-<div class="container-fluid">
-    <div class="row">
-        <?php $i = 0;
-        foreach ($videos as $video) {
-            $i++; ?>
-            <div class="col-md">
-                <div class="card mt-4" style="width: 16rem">
-                    <img src="images/img-algo2.png" height="270" alt="" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">Chapitre<?= $i; ?> </h5>
-
-                        <a class="venobox_custom " data-vbtype="iframe"
-                           href="https://www.youtube.com/watch?v=ebx-tGjaVoU">
-                            <div class="card-footer">
-                                Regarder
-                            </div>
-                        </a>
-
-                    </div>
+<!--<div class="container-fluid">-->
+<!--    <div class="row">-->
+<!--        --><?php //$i = 0;
+//        foreach ($videos as $video) {
+//            $i++; ?>
+<!--            <div class="col-md">-->
+<!--                <div class="card mt-4" style="width: 16rem">-->
+<!--                    <img src="images/cour.png" height="270" alt="" class="card-img-top">-->
+<!--                    <div class="card-body">-->
+<!--                        <h5 class="card-title">Chapitre--><? //= $i; ?><!-- </h5>-->
+<!--                        <a class="venobox" data-autoplay="true" data-vbtype="video"-->
+<!--                           href="https://youtu.be/0z5CRP9VMMo">-->
+<!--                            <div class="card-footer">-->
+<!--                                Regarder-->
+<!--                            </div>-->
+<!--                        </a>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        --><?php //} ?>
+<!--    </div>-->
+<!--</div>-->
+<!-- Video Gallery - START -->
+<div class="container-fluid pb-video-container">
+    <div class="col-md-12 col-md-offset-1">
+        <div class="row pb-row">
+            <div class="col-md-3 pb-video">
+                <div class="preview-container">
+                    <img src="images/cour.png" width="100%" height="230" alt="algo">
+                    <a class="venobox" data-autoplay="true" data-vbtype="video" href="https://youtu.be/0z5CRP9VMMo">
+                        <i class="fa fa-play" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="abstract text-justify card-custumer">
+                    <p class="text-center font-italic text-underline"><strong>Chapitre 1 : Résumé</strong></p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur, eius eum ex in
+                    maxime nihil quidem quod. Ab blanditiis commodi eius exercitationem facilis, itaque odit saepe
+                    tempore voluptates. Perferendis. Lorem ipsum dolor sit amet, consectetur adipisicing elit ...
                 </div>
             </div>
-        <?php } ?>
-        <a class="venobox" data-vbtype="" href="https://www.youtube.com/watch?v=NOqX94_3z-g">YouYbe</a>
+            <div class="col-md-3 pb-video">
+                <div class="preview-container">
+                    <img src="images/cour.png" width="100%" height="230" alt="algo">
+                    <a class="venobox" data-autoplay="true" data-vbtype="video" href="https://youtu.be/0z5CRP9VMMo">
+                        <i class="fa fa-play" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="abstract text-justify card-custumer">
+                    <p class="text-center font-italic text-underline"><strong>Chapitre 1 : Résumé</strong></p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur, eius eum ex in
+                    maxime nihil quidem quod. Ab blanditiis commodi eius exercitationem facilis, itaque odit saepe
+                    tempore voluptates. Perferendis. Lorem ipsum dolor sit amet, consectetur adipisicing elit ...
+                </div>
+            </div>
+            <div class="col-md-3 pb-video">
+                <div class="preview-container">
+                    <img src="images/cour.png" width="100%" height="230" alt="algo">
+                    <a class="venobox" data-autoplay="true" data-vbtype="video" href="https://youtu.be/0z5CRP9VMMo">
+                        <i class="fa fa-play" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="abstract text-justify card-custumer">
+                    <p class="text-center font-italic text-underline"><strong>Chapitre 1 : Résumé</strong></p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur, eius eum ex in
+                    maxime nihil quidem quod. Ab blanditiis commodi eius exercitationem facilis, itaque odit saepe
+                    tempore voluptates. Perferendis. Lorem ipsum dolor sit amet, consectetur adipisicing elit ...
+                </div>
+            </div>
+            <div class="col-md-3 pb-video">
+                <div class="preview-container">
+                    <img src="images/cour.png" width="100%" height="230" alt="algo">
+                    <a class="venobox" data-autoplay="true" data-vbtype="video" href="https://youtu.be/0z5CRP9VMMo">
+                        <i class="fa fa-play" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="abstract text-justify card-custumer">
+                    <p class="text-center font-italic text-underline"><strong>Chapitre 1 : Résumé</strong></p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur, eius eum ex in
+                    maxime nihil quidem quod. Ab blanditiis commodi eius exercitationem facilis, itaque odit saepe
+                    tempore voluptates. Perferendis. Lorem ipsum dolor sit amet, consectetur adipisicing elit ...
+                </div>
+            </div>
+        </div>
+        <div class="row pb-row">
+            <div class="col-md-3 pb-video">
+                <div class="preview-container">
+                    <img src="images/cour.png" width="100%" height="230" alt="algo">
+                    <a class="venobox" data-autoplay="true" data-vbtype="video" href="https://youtu.be/0z5CRP9VMMo">
+                        <i class="fa fa-play" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="abstract text-justify card-custumer">
+                    <p class="text-center font-italic text-underline"><strong>Chapitre 1 : Résumé</strong></p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur, eius eum ex in
+                    maxime nihil quidem quod. Ab blanditiis commodi eius exercitationem facilis, itaque odit saepe
+                    tempore voluptates. Perferendis. Lorem ipsum dolor sit amet, consectetur adipisicing elit ...
+                </div>
+            </div>
+            <div class="col-md-3 pb-video">
+                <div class="preview-container">
+                    <img src="images/cour.png" width="100%" height="230" alt="algo">
+                    <a class="venobox" data-autoplay="true" data-vbtype="video" href="https://youtu.be/0z5CRP9VMMo">
+                        <i class="fa fa-play" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="abstract text-justify card-custumer">
+                    <p class="text-center font-italic text-underline"><strong>Chapitre 1 : Résumé</strong></p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur, eius eum ex in
+                    maxime nihil quidem quod. Ab blanditiis commodi eius exercitationem facilis, itaque odit saepe
+                    tempore voluptates. Perferendis. Lorem ipsum dolor sit amet, consectetur adipisicing elit ...
+                </div>
+            </div>
+            <div class="col-md-3 pb-video">
+                <div class="preview-container">
+                    <img src="images/cour.png" width="100%" height="230" alt="algo">
+                    <a class="venobox" data-autoplay="true" data-vbtype="video" href="https://youtu.be/0z5CRP9VMMo">
+                        <i class="fa fa-play" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="abstract text-justify card-custumer">
+                    <p class="text-center font-italic text-underline"><strong>Chapitre 1 : Résumé</strong></p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur, eius eum ex in
+                    maxime nihil quidem quod. Ab blanditiis commodi eius exercitationem facilis, itaque odit saepe
+                    tempore voluptates. Perferendis. Lorem ipsum dolor sit amet, consectetur adipisicing elit ...
+                </div>
+            </div>
+            <div class="col-md-3 pb-video">
+                <div class="preview-container">
+                    <img src="images/cour.png" width="100%" height="230" alt="algo">
+                    <a class="venobox" data-autoplay="true" data-vbtype="video" href="https://youtu.be/0z5CRP9VMMo">
+                        <i class="fa fa-play" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div class="abstract text-justify card-custumer">
+                    <p class="text-center font-italic text-underline"><strong>Chapitre 1 : Résumé</strong></p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur, eius eum ex in
+                    maxime nihil quidem quod. Ab blanditiis commodi eius exercitationem facilis, itaque odit saepe
+                    tempore voluptates. Perferendis. Lorem ipsum dolor sit amet, consectetur adipisicing elit ...
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
+<style>
+    .pb-video-container {
+        padding-top: 20px;
+    }
 
+    .abstract {
+        /*border: 2px solid rgba(0, 0, 0, 0.5);*/
+        padding: 1rem;
+    }
+
+    .text-underline {
+        text-decoration: underline;
+    }
+
+    .pb-video {
+        padding: 5px;
+    }
+
+    .preview-container {
+        position: relative;
+    }
+
+    .fa.fa-play {
+        position: absolute;
+        color: rgba(0, 0, 0, 0.3);
+        z-index: 999;
+        font-size: 5rem;
+        top: 38%;
+        left: 41%;
+    }
+
+    /*.pb-video:hover {*/
+    /*    background: #2c3e50;*/
+    /*}*/
+
+    .pb-row {
+        margin-bottom: 10px;
+    }
+</style>
+
+<!-- Video Gallery - END -->
--- views/template/top.php
+++ views/template/top.php
@@ -7,12 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" type="image/png" href="<?= buildpath("images/logo.png") ?>"/>
     <!-- Stylesheet   -->
-    <link rel="stylesheet" href='<?= buildpath("public/css/bootstrap&mdb.css") ?>'>
+    <link rel="stylesheet" href='<?= buildpath("public/css/import/bootstrap-mdb.css") ?>'>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700&display=swap"
           rel="stylesheet">
     <link rel="stylesheet" href='<?= buildpath("public/css/style.css") ?>'>
     <?= suppl_tags($links ?? [], LINK) ?>
+    <!-- Stylesheet   -->
     <title><?= $title ?? "Title" ?></title>
 </head>
 <body>
