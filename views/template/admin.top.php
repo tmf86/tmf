571dab22cd018a70c6c057325abe3209d6b8bbba
tmf86 <touremarc25@gmail.com>
2021-05-24 00:31:06 +0000

    maj log admin

--- views/template/admin.top.php
+++ views/template/admin.top.php
@@ -187,8 +187,8 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="index.php" class="brand-link">
-            <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <a href="home_admin" class="brand-link">
+            <img src="<?= makeRootOrFileUrl("images/cpy.jpeg"); ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">CIPY Admin</span>
         </a>
 
@@ -197,7 +197,7 @@
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                    <img src="<?= makeRootOrFileUrl("images/cpy.jpeg"); ?>" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
                     <a href="#" class="d-block">Samy & Tmf</a>
